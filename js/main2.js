c98b978926c9b1fd9272e9ef5123a53c535a9aea
petr-micola <petamicol@gmail.com>
2021-11-25 14:26:26 +0100

    Oprava chyb

--- js/main2.js
+++ js/main2.js
@@ -1,13 +1,16 @@
 const btn = document.getElementById('btn');
-const score = document.getElementById('skore');
 
+let p1a = [];
+let p2a = [];
+let stats;
+let score;
 let cube;
 let kolo = 0;
 let timer;
 let p1;
 let p2;
 
-function animation () {
+function animation() {
     if (kolo % 2 == 0) {
         p1 = Math.ceil(Math.random() * 6);
         cube.src = `img/${p1}.svg`;
@@ -17,16 +20,60 @@ function animation () {
     }
 }
 
-function cancel () {
+function sum() {
+    let total = 0;
+    if (kolo % 2 == 0) {
+        p1a.forEach(function (value) {
+            total += value;
+        });
+    } else {
+        p2a.forEach(function (value) {
+            total += value;
+        });
+    }
+    return total;
+}
+
+function max() {
+    let maximum = 1;
+    p1a.forEach(function (value) {
+        if (value > maximum) maximum = value;
+    });
+    return maximum;
+}
+
+function min() {
+    let minimum = 6;
+    p1a.forEach(function (value) {
+        if (value < minimum) minimum = value;
+    });
+    return minimum
+}
+
+function cancel() {
     clearInterval(timer);
-    if (kolo % 2 == 0) score.innerHTML += `<p>${kolo+1}. Hráč 1 hodil ${p1}.</p>`;
-    else score.innerHTML += `<p>${kolo+1}. Hráč 2 hodil ${p2}.</p>`;
+    if (kolo % 2 == 0) {
+        p1a.push(p1);
+        score.innerHTML += `<p>${p1}</p>`;
+        stats.innerHTML = `<p>${p1a.length}</p>`;
+    } else {
+        p2a.push(p2);
+        score.innerHTML += `<p>${p2}</p>`;
+        stats.innerHTML = `<p>${p2a.length}</p>`;
+    }
     kolo++;
 }
 
 function game() {
-    if (kolo % 2 == 0) cube = document.getElementById('cube1');
-    else cube = document.getElementById('cube2');
+    if (kolo % 2 == 0) {
+        cube = document.getElementById('cube1');
+        score = document.getElementById('skore1');
+        stats = document.getElementById('statistika1');
+    } else {
+        cube = document.getElementById('cube2');
+        score = document.getElementById('skore2');
+        stats = document.getElementById('statistika2');
+    }
     timer = setInterval(animation, 180);
     setTimeout(cancel, 1500);
 }
